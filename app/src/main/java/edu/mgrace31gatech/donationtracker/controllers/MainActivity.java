056186d8b42170d21558410899000ce97eb26117
Marie Grace <mgrace31@gatech.edu>
2018-09-20 11:19:27 -0400

    added controllers and models (to look like what we did in M3)

--- app/src/main/java/edu/mgrace31gatech/donationtracker/controllers/MainActivity.java
+++ app/src/main/java/edu/mgrace31gatech/donationtracker/controllers/MainActivity.java
@@ -1,7 +1,9 @@
-package edu.mgrace31gatech.donationtracker;
+package edu.mgrace31gatech.donationtracker.controllers;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import edu.mgrace31gatech.donationtracker.R;
 
 public class MainActivity extends AppCompatActivity {
 
