4817cdc69e245a4c871c96350f5c102d4759c33d
Andrew Dallas <dadallas1202@gmail.com>
2018-10-15 11:38:52 -0400

    Made a DonationActivity

---
+++ app/src/main/java/edu/mgrace31gatech/donationtracker/DonationActivity.java
@@ -0,0 +1,14 @@
+package edu.mgrace31gatech.donationtracker;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class DonationActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donation);
+    }
+
+}
