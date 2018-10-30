219524e8e0411052a6de08acd45342a7d7aae502
Marie Grace <mgrace31@gatech.edu>
2018-10-29 21:18:22 -0400

    have all the components for search there. List shows locations, not donations, and no search functionality yet

--- app/src/main/java/edu/mgrace31gatech/donationtracker/app/controllers/SearchActivity.java
+++ app/src/main/java/edu/mgrace31gatech/donationtracker/app/controllers/SearchActivity.java
@@ -1,12 +1,19 @@
 package edu.mgrace31gatech.donationtracker.app.controllers;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.AdapterView;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,11 +23,12 @@ import edu.mgrace31gatech.donationtracker.app.model.Donation;
 import edu.mgrace31gatech.donationtracker.app.model.Location;
 import edu.mgrace31gatech.donationtracker.app.model.LocationsModel;
 
-public class SearchActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+public class SearchActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+//        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
 
@@ -29,13 +37,13 @@ public class SearchActivity extends AppCompatActivity implements AdapterView.OnI
         Spinner catSpinner = (Spinner) findViewById(R.id.catSpinner);
 
 
-        List<Location> locations = LocationsModel.INSTANCE.getItems();
+        List<Location> locations1 = LocationsModel.INSTANCE.getItems();
         List<String> locationName = new ArrayList<>();
         List<Donation> allDonations = new ArrayList<>();
         locationName.add("All Locations");
 
         // make a list of locations as well as list of all donations
-        for (Location l : locations) {
+        for (Location l : locations1) {
             locationName.add(l.getName());
             for (Donation d : l.getInventory()) {
                 allDonations.add(d);
@@ -50,6 +58,8 @@ public class SearchActivity extends AppCompatActivity implements AdapterView.OnI
             catergories2.add(c);
         }
 
+
+
         ArrayAdapter<String> adapter = new ArrayAdapter(this,android.R.layout.simple_spinner_item, locationName);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         locationSpinner.setAdapter(adapter);
@@ -57,87 +67,78 @@ public class SearchActivity extends AppCompatActivity implements AdapterView.OnI
         ArrayAdapter<String> adapter2 = new ArrayAdapter(this,android.R.layout.simple_spinner_item, catergories2);
         adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         catSpinner.setAdapter(adapter2);
+        super.onCreate(savedInstanceState);
 
-        View recyclerView = findViewById(R.id.donationRecycle);
+
+        
+        View recyclerView = findViewById(R.id.dataitem_list);
         assert recyclerView != null;
-//        setupRecyclerView((RecyclerView) recyclerView);
+        setupRecyclerView((RecyclerView) recyclerView);
     }
-//    private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
-//        recyclerView.setAdapter(new LocationList.SimpleItemRecyclerViewAdapter(LocationsModel.INSTANCE.getItems()));
-//    }
-//    public class SimpleItemRecyclerViewAdapter
-//            extends RecyclerView.Adapter<LocationList.SimpleItemRecyclerViewAdapter.ViewHolder> {
-//
-//        private final List<Location> mValues;
-//
-//        public SimpleItemRecyclerViewAdapter(List<Location> locations) {
-//            mValues = locations;
-//        }
-//
-//        @Override
-//        public LocationList.SimpleItemRecyclerViewAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-//            View view = LayoutInflater.from(parent.getContext())
-//                    .inflate(R.layout.location_list_content, parent, false);
-//            return new LocationList.SimpleItemRecyclerViewAdapter.ViewHolder(view);
-//        }
-//
-//
-//        @Override
-//        public int getItemCount() {
-//            return mValues.size();
-//        }
-//
-//        @Override
-//        public void onBindViewHolder(final LocationList.SimpleItemRecyclerViewAdapter.ViewHolder holder, int position) {
-//            holder.mItem = mValues.get(position);
-//            holder.mKeyView.setText("" + mValues.get(position).getKey());
-//            holder.mContentView.setText(mValues.get(position).getName());
-//
-//            final LocationsModel model = LocationsModel.getInstance();
-//
-//            holder.mView.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View v) {
-//                    Context context = v.getContext();
-//                    Intent intent = new Intent(context, LocationDetailActivity.class);
-//                    Log.d("MYAPP", "Switch to detailed view for item: " + holder.mItem.getName());
-//                    Log.d("MYAPP", "Switch to detailed view for item: " + holder.mItem.getKey());
-//                    intent.putExtra(LocationDetailFragment.ARG_ITEM_ID, holder.mItem.getKey());
-//                    model.setCurrentLocation(holder.mItem);
-//                    context.startActivity(intent);
-//                }
-//            });
-//        }
-//
-//        public class ViewHolder extends RecyclerView.ViewHolder {
-//            public final View mView;
-//            public final TextView mKeyView;
-//            public final TextView mContentView;
-//            public Location mItem;
-//
-//            public ViewHolder(View view) {
-//                super(view);
-//                mView = view;
-//                mKeyView = (TextView) view.findViewById(R.id.id);
-//                mContentView = (TextView) view.findViewById(R.id.content);
-//            }
-//
-//            @Override
-//            public String toString() {
-//                return super.toString() + " '" + mContentView.getText() + "'";
-//            }
-//        }
-//    }
-
-
-
-    @Override
-    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-
+    private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
+        recyclerView.setAdapter(new SimpleItemRecyclerViewAdapter(LocationsModel.INSTANCE.getItems()));
     }
+    public static class SimpleItemRecyclerViewAdapter
+            extends RecyclerView.Adapter<SimpleItemRecyclerViewAdapter.ViewHolder> {
 
-    @Override
-    public void onNothingSelected(AdapterView<?> adapterView) {
+        private final List<Location> mValues;
 
+        public SimpleItemRecyclerViewAdapter(List<Location> locations) {
+            mValues = locations;
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.location_list_content, parent, false);
+            return new ViewHolder(view);
+        }
+
+
+        @Override
+        public int getItemCount() {
+            return mValues.size();
+        }
+
+        @Override
+        public void onBindViewHolder(final ViewHolder holder, int position) {
+            holder.mItem = mValues.get(position);
+            holder.mKeyView.setText("" + mValues.get(position).getKey());
+            holder.mContentView.setText(mValues.get(position).getName());
+
+            final LocationsModel model = LocationsModel.getInstance();
+
+            holder.mView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Context context = v.getContext();
+                    Intent intent = new Intent(context, LocationDetailActivity.class);
+                    Log.d("MYAPP", "Switch to detailed view for item: " + holder.mItem.getName());
+                    Log.d("MYAPP", "Switch to detailed view for item: " + holder.mItem.getKey());
+                    intent.putExtra(LocationDetailFragment.ARG_ITEM_ID, holder.mItem.getKey());
+                    model.setCurrentLocation(holder.mItem);
+                    context.startActivity(intent);
+                }
+            });
+        }
+
+        public static class ViewHolder extends RecyclerView.ViewHolder {
+            public final View mView;
+            public final TextView mKeyView;
+            public final TextView mContentView;
+            public Location mItem;
+
+            public ViewHolder(View view) {
+                super(view);
+                mView = view;
+                mKeyView = (TextView) view.findViewById(R.id.id);
+                mContentView = (TextView) view.findViewById(R.id.content);
+            }
+
+            @Override
+            public String toString() {
+                return super.toString() + " '" + mContentView.getText() + "'";
+            }
+        }
     }
 }
