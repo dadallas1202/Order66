39115070128f1f40608cbc04d595610b49351eef
alaynapanlilio <42749768+alaynapanlilio@users.noreply.github.com>
2018-11-01 14:18:08 -0400

    Modified persistence to work for adding multiple users and added implementation for maps activity in gradle file and manifest

--- app/src/main/java/edu/mgrace31gatech/donationtracker/app/controllers/MapsActivity.java
+++ app/src/main/java/edu/mgrace31gatech/donationtracker/app/controllers/MapsActivity.java
@@ -1,4 +1,4 @@
-package edu.mgrace31gatech.donationtracker;
+package edu.mgrace31gatech.donationtracker.app.controllers;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
@@ -9,6 +9,8 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import edu.mgrace31gatech.donationtracker.R;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
--- app/src/main/java/edu/mgrace31gatech/donationtracker/app/model/RegisteredUser.java
+++ app/src/main/java/edu/mgrace31gatech/donationtracker/app/model/RegisteredUser.java
@@ -1,5 +1,10 @@
 package edu.mgrace31gatech.donationtracker.app.model;
 
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import com.google.gson.Gson;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -25,11 +30,6 @@ public class RegisteredUser {
         this.isAdmin = isAdmin;
     }
 
-
-    public static Map<String, String> usersList() {
-        return users;
-    }
-
     public String getUserName() {
         return userName;
     }
@@ -37,4 +37,34 @@ public class RegisteredUser {
     public String getPassword() {
         return password;
     }
+
+    public static Map<String, String> getUsers() {
+        return users;
+    }
+
+    public static void setMyUsers(List<RegisteredUser> list) {
+        if (list == null) {
+            myUsers = new ArrayList<>();
+        }
+         else {
+            myUsers = list;
+        }
+    }
+
+    public static List<RegisteredUser> getMyUsers() {
+        return myUsers;
+    }
+
+    public static boolean addUser(String name, String userName, String password, String userType) {
+        RegisteredUser newUser = userType.equals("User")
+                ? new User(name, userName, password)
+                : new Admin(name, userName, password);
+        if (!users.containsKey(userName)) {
+            myUsers.add(newUser);
+            users.put(userName, password);
+            return true;
+        }
+        return false;
+    }
+
 }
