7d16178505afa31ec0496becb7b0393066e318b1
Andrew Dallas <dadallas1202@gmail.com>
2018-11-14 20:24:54 -0500

    Deleted Model.java

--- app/src/main/java/edu/mgrace31gatech/donationtracker/app/controllers/RegistrationActivity.java
+++ app/src/main/java/edu/mgrace31gatech/donationtracker/app/controllers/RegistrationActivity.java
@@ -15,15 +15,10 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 import java.lang.reflect.Type;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import edu.mgrace31gatech.donationtracker.R;
-import edu.mgrace31gatech.donationtracker.app.model.Admin;
-import edu.mgrace31gatech.donationtracker.app.model.Model;
 import edu.mgrace31gatech.donationtracker.app.model.RegisteredUser;
-import edu.mgrace31gatech.donationtracker.app.model.User;
 
 public class RegistrationActivity extends AppCompatActivity {
 
