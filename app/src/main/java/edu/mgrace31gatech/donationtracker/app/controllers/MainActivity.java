ec9d4d822c7e5e644998fb3a767c4100d23809f0
Andrew Dallas <dadallas1202@gmail.com>
2018-11-14 21:31:14 -0500

    More format stuff

--- app/src/main/java/edu/mgrace31gatech/donationtracker/app/controllers/MainActivity.java
+++ app/src/main/java/edu/mgrace31gatech/donationtracker/app/controllers/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_page);
-        Button login = findViewById(R.id.welcomeloginButton);
+        Button login = findViewById(R.id.welcomeLoginButton);
         Button register = findViewById(R.id.registerButton);
 
         login.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/edu/mgrace31gatech/donationtracker/app/controllers/WelcomePageActivity.java
+++ app/src/main/java/edu/mgrace31gatech/donationtracker/app/controllers/WelcomePageActivity.java
@@ -17,7 +17,7 @@ public class WelcomePageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_page);
-        Button login = findViewById(R.id.welcomeloginButton);
+        Button login = findViewById(R.id.welcomeLoginButton);
         Button register = findViewById(R.id.registerButton);
 
         login.setOnClickListener(new View.OnClickListener() {
