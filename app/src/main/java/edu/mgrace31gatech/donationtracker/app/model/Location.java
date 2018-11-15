7508e4edba4023dcce6ed5d0e4bd65ec193aea60
juliatang <juliatmay@gmail.com>
2018-11-14 21:39:14 -0500

    Javadocs for the RegisteredUser class.

--- app/src/main/java/edu/mgrace31gatech/donationtracker/app/model/Location.java
+++ app/src/main/java/edu/mgrace31gatech/donationtracker/app/model/Location.java
@@ -21,17 +21,18 @@ public class Location {
 
     /**
      * Creates a location.
-     * @param k The key of the location.
-     * @param n The name of the location.
+     *
+     * @param k   The key of the location.
+     * @param n   The name of the location.
      * @param lat The latitude of the location.
      * @param lon The longitude of the location.
-     * @param a The address of the location.
-     * @param c The city of the location.
-     * @param s The state of the location.
-     * @param z The zipcode of the location.
-     * @param t The type of the location.
-     * @param p The phone number of the location.
-     * @param w The website of the location.
+     * @param a   The address of the location.
+     * @param c   The city of the location.
+     * @param s   The state of the location.
+     * @param z   The zipcode of the location.
+     * @param t   The type of the location.
+     * @param p   The phone number of the location.
+     * @param w   The website of the location.
      */
     public Location(int k, String n, String lat, String lon, String a, String c, String s,
                     String z, String t, String p, String w) {
@@ -51,6 +52,7 @@ public class Location {
 
     /**
      * Gets the name of the location.
+     *
      * @return The name of the location.
      */
     public String getName() {
@@ -59,6 +61,7 @@ public class Location {
 
     /**
      * Gets the key of the location.
+     *
      * @return The key of the location.
      */
     public int getKey() {
@@ -67,79 +70,88 @@ public class Location {
 
     /**
      * Gets the type of the location.
+     *
      * @return The type of the location.
      */
     public String getType() {
-    public CharSequence getType() {
-        return type;
+        public CharSequence getType () {
+            return type;
+        }
+
+        /**
+         * Gets the latitude of the location.
+         * @return The latitude of the location.
+         */
+        public String getLatitude () {
+            return latitude;
+        }
+
+        /**
+         * Gets the longitude of the location.
+         * @return The longitude of the location.
+         */
+        public String getLongitude () {
+            return longitude;
+        }
+
+        /**
+         * Gets the phone number of the location.
+         * @return The phone number of the location.
+         */
+        public String getPhone () {
+            return phone;
+        }
+
+        public CharSequence getAddress () {
+            /**
+             * Gets the address of the location.
+             * @return The address of the location.
+             */
+            public String getAddress () {
+                return address;
+            }
+
+            /**
+             * Gets the inventory of the location.
+             * @return The inventory list of donations for the location.
+             */
+            public List<Donation> getInventory () {
+                return Collections.unmodifiableList(inventory);
+            }
+
+            /**
+             * Sets the inventory of the location.
+             * @param donationList The inventory list of donations for the location.
+             */
+            public void setInventory (List < Donation > donationList) {
+                inventory = donationList;
+            }
+
+            /**
+             * Adds a donation to the inventory of the location.
+             * @param d The donation to be added.
+             */
+            public void addDonation (Donation d){
+                inventory.add(d);
+            }
+
+            /**
+             * Gets the size of the inventory.
+             * @return The size of the inventory of the location.
+             */
+            public int size () {
+                return inventory.size();
+            }
+
+            /**
+             * Converts the name of the location to a string.
+             * @return The name of the location.
+             */
+            @Override
+            public String toString() {
+                return name;
+            }
+
+        }
     }
-
-    /**
-     * Gets the latitude of the location.
-     * @return The latitude of the location.
-     */
-    public String getLatitude() {
-        return latitude;
-    }
-
-    /**
-     * Gets the longitude of the location.
-     * @return The longitude of the location.
-     */
-    public String getLongitude() {
-        return longitude;
-    }
-
-    /**
-     * Gets the phone number of the location.
-     * @return The phone number of the location.
-     */
-    public String getPhone() {
-        return phone;
-    }
-
-    public CharSequence getAddress() {
-    /**
-     * Gets the address of the location.
-     * @return The address of the location.
-     */
-    public String getAddress() {
-        return address;
-    }
-
-    /**
-     * Gets the inventory of the location.
-     * @return The inventory list of donations for the location.
-     */
-    public List<Donation> getInventory() { return Collections.unmodifiableList(inventory); }
-
-    /**
-     * Sets the inventory of the location.
-     * @param donationList The inventory list of donations for the location.
-     */
-    public void setInventory(List<Donation> donationList) { inventory = donationList; }
-
-    /**
-     * Adds a donation to the inventory of the location.
-     * @param d The donation to be added.
-     */
-    public void addDonation(Donation d) {
-        inventory.add(d);
-    }
-
-    /**
-     * Gets the size of the inventory.
-     * @return The size of the inventory of the location.
-     */
-    public int size() {
-        return inventory.size();
-    }
-
-    /**
-     * Converts the name of the location to a string.
-     * @return The name of the location.
-     */
-    @Override
-    public String toString() { return name; }
-    
 }
