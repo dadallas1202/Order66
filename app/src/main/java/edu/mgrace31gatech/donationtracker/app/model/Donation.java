78841918417fc6c35739464efe62b69e19ad0fb2
juliatang <juliatmay@gmail.com>
2018-11-14 21:04:56 -0500

    Javadocs for Donation class.

--- app/src/main/java/edu/mgrace31gatech/donationtracker/app/model/Donation.java
+++ app/src/main/java/edu/mgrace31gatech/donationtracker/app/model/Donation.java
@@ -137,55 +137,132 @@ public class Donation implements Parcelable {
      */
     public void setCategory(String _category) { category = _category; }
 
+    /**
+     * Gets the comments of the donation.
+     * @return The comments of the donation.
+     */
     public String getComments() { return comments; }
 
+    /**
+     * Gets the date the donation was added.
+     * @return The date the donation was added.
+     */
     public String getDate() {
-        //DateFormat dateFormat = new SimpleDateFormat("MM-dd-yyyy");
-        //return dateFormat.format(date);
         return date.toString();
     }
+
+    /**
+     * Sets the date the donation was added.
+     * @param _date The date the donation was added.
+     */
     public void setDate(LocalDate _date) { date = _date; }
 
+    /**
+     * Gets the long description of the donation.
+     * @return The long description of the donation.
+     */
     public String getLongDescription() { return long_description; }
+
+    /**
+     * Sets the long description of the donation.
+     * @param _longDescription The long description of the donation.
+     */
     public void setLongDescription(String _longDescription) { long_description = _longDescription; }
 
+    /**
+     * Gets the name of the donation.
+     * @return The name of the donation.
+     */
     public String getName() { return name; }
+
+    /**
+     * Sets the name of the donation.
+     * @param _name The name of the donation.
+     */
     public void setName(String _name) { name = _name; }
 
+    /**
+     * Gets the short description of the donation.
+     * @return The short description of the donation.
+     */
     public String getShortDescription() { return short_description; }
+
+    /**
+     * Sets the short description of the donation.
+     * @param _shortDescription The short description of the donation.
+     */
     public void setShortDescription(String _shortDescription) { short_description = _shortDescription; }
 
+    /**
+     * Get the time the donation was added.
+     * @return The time the donation was added.
+     */
     public String getTime() {
-        //DateFormat dateFormat = new SimpleDateFormat("hh:mm:ss");
-        //return dateFormat.format(time);
         return time.toString();
     }
+
+    /**
+     * Sets the time the donation was added.
+     * @param _time The time the donation was added.
+     */
     public void setTime(LocalTime _time) { time = _time; }
 
+    /**
+     * Gets the value of the donation.
+     * @return The value of the donation.
+     */
     public double getValue() { return value; }
+
+    /**
+     * Sets the value of the donation.
+     * @param _value The value of the donation.
+     */
     public void setValue(double _value) { value = _value; }
 
+    /**
+     * Gets the id of the donation.
+     * @return The id of the donation.
+     */
     public int getId() { return id; }
 
+    /**
+     * Gets the view id of the donation.
+     * @return The view id of the donation.
+     */
     public int getViewId() {
         return viewId;
     }
 
+    /**
+     * Sets the view id of the donation.
+     * @param viewId The view id of the donation.
+     */
     public void setViewId(int viewId) {
         this.viewId = viewId;
     }
 
+    /**
+     * Sets the location of the donation.
+     * @param location The location of the donation.
+     */
     public void setLocation(Location location) {
         Location location1 = location;
     }
 
 
-
+    /**
+     * Converts the name of the donation to a string.
+     * @return The name of the donation.
+     */
     @Override
     public String toString() {
         return name;
     }
 
+    /**
+     * Reads in the donation parcel.
+     * @param in The donation parcel.
+     */
     private Donation(Parcel in) {
         name = in.readString();
         short_description = in.readString();
@@ -194,9 +271,18 @@ public class Donation implements Parcelable {
         category = in.readString();
     }
 
+    /**
+     * Describe the contents of the donation.
+     * @return The number of contents.
+     */
     @Override
     public int describeContents() { return 0; }
 
+    /**
+     * Write the donation to parcel.
+     * @param dest Destination of parcel.
+     * @param flags Number of flags.
+     */
     @Override
     public void writeToParcel(Parcel dest, int flags) {
         dest.writeString(name);
@@ -206,10 +292,24 @@ public class Donation implements Parcelable {
         dest.writeString(category);
     }
 
+    /**
+     * Creates a parcel for donation.
+     */
     public static final Parcelable.Creator<Donation> CREATOR
             = new Parcelable.Creator<Donation>() {
+        /**
+         * Creates a parcel for specific donation.
+         * @param in The parameters for the donation.
+         * @return The donation created.
+         */
         @Override
         public Donation createFromParcel(Parcel in) {return new Donation(in); }
+
+        /**
+         * Create a new array of donations.
+         * @param size The size of the array of donations.
+         * @return The donation array.
+         */
         @Override
         public Donation[] newArray(int size) { return new Donation[size]; }
     };
